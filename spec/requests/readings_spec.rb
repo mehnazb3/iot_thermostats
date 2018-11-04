b20b3eba79d43f5bc2d8f1126840d1217f58d1fb
Mehnaz Bano <mehnaz.bano@tcs.com>
2018-11-04 23:47:55 +0530

    IOT Thermostat: Minor fix in request tests
     * Fixed before filter for reading tests.

--- spec/requests/readings_spec.rb
+++ spec/requests/readings_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Readings", :type => :request do
-  describe "GET /readings" do
+  describe "GET /readings/1" do
     it "works! (now write some real specs)" do
-      # TO DO
-      # get api_v1_readings_path
-      # expect(response).to have_http_status(200)
+      get api_v1_reading_path(1)
+      expect(response).to have_http_status(401)
     end
   end
 end
